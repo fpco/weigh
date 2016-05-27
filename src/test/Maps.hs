db25ca71cb31152c328f8cfbdc90259037e6585e
Chris Done <chrisdone@gmail.com>
2016-05-27 18:05:48 +0200

    Switch to Int

--- src/test/Maps.hs
+++ src/test/Maps.hs
@@ -8,6 +8,8 @@ module Main where
 import           Control.DeepSeq
 import qualified Data.HashMap.Lazy
 import qualified Data.HashMap.Strict
+import qualified Data.IntMap.Lazy
+import qualified Data.IntMap.Strict
 import qualified Data.Map.Lazy
 import qualified Data.Map.Strict
 import           System.Random
@@ -36,9 +38,11 @@ inserts = do func "Data.Map.Strict.insert mempty"
 fromlists :: Weigh ()
 fromlists =
   do let !elems =
-           force (zip (randoms (mkStdGen 0) :: [Char])
+           force (zip (randoms (mkStdGen 0) :: [Int])
                       [1 :: Int .. 1000000])
      func "Data.Map.Strict.fromList     (1 million)" Data.Map.Strict.fromList elems
      func "Data.Map.Lazy.fromList       (1 million)" Data.Map.Lazy.fromList elems
+     func "Data.IntMap.Strict.fromList  (1 million)" Data.IntMap.Strict.fromList elems
+     func "Data.IntMap.Lazy.fromList    (1 million)" Data.IntMap.Lazy.fromList elems
      func "Data.HashMap.Strict.fromList (1 million)" Data.HashMap.Strict.fromList elems
      func "Data.HashMap.Lazy.fromList   (1 million)" Data.HashMap.Lazy.fromList elems
